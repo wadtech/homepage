8c2955d74c503f4a5cbb4e13ba502f159320bf31
Peter Mellett <bulletraven@gmail.com>
2011-12-22 19:20:48 +0000

    Articles format correctly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,4 +80,5 @@ class ArticlesController < ApplicationController
       format.json { head :ok }
     end
   end
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   # eventually has_one author
 
-  validates :content, :author, :title, :published, :presence => true
+  validates :content, :author, :title, :presence => true
+  validates :published, :inclusion => {:in => [true, false]}
+
 
 end
