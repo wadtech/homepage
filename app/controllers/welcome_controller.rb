7bd11310b0669ed59815ea583c97dfe006bb5208
Peter Mellett <bulletraven@gmail.com>
2012-01-09 22:10:29 +0000

    updating welcome controller layout and related partials

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,21 @@
 class WelcomeController < ApplicationController
   def index
     # get latest 5 articles in summary form
+    # only if they're published though!
+    @articles = Article.limit(5).order('created_at desc').where(:published => true)
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
     end    
+  end
+
+  def show_article
+    @article = Article.get(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @articles }
+    end
   end
 end
