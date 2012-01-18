4a6f00bad2d0a7f7ef07f4b0533331f3c4712a5e
Peter Mellett <bulletraven@gmail.com>
2012-01-18 13:06:42 +0000

    emptied pre-generated tests and updated factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,20 @@
 require 'factory_girl'
 
-Factory.define :admin do |u|
-  u.email 'user@test.com'
-  u.password 'please'
-  u.password_confirmation 'please'
+Factory.define do
+  factory :admin do
+    email 'user@test.com'
+    password 'please'
+  end
+
+  factory :article do
+    title 'Sample Title'
+    author 'Test Author'
+    content 'Sample content'
+    published false
+
+    factory :published_article do
+        published true
+    end
+  end
 end
 
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe Article do
-      before(:each) do
-    @attr = { 
-      :title => "Sample Title",
-      :author => "Author",
-      :content => "Sample content of multiple characters.",
-      :published => true,
-    }
-  end
-  
-  it "should create a new instance given a valid attribute" do
-    Admin.create!(@attr)
-  end
+
 end
