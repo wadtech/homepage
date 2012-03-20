c129c2951cd26daa08711f7168c48917d42157dc
Peter Mellett <bulletraven@gmail.com>
2012-03-20 19:19:22 +0000

    changed production tld to 2 (.co.uk)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Homepage::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_dispatch.tld_length = 2
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
