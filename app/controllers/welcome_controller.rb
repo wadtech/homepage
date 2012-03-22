91019e3dfcc4fccfa6938edb8f0a24957f24c634
Peter Mellett <bulletraven@gmail.com>
2012-03-22 20:56:28 +0000

    removed search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,20 +2,18 @@ class WelcomeController < ApplicationController
   def index
     # get latest 5 articles in summary form
     # only if they're published though!
-    # if params[:search].nil?
-    #   @articles = Article.paginate(:per_page => 1, :page => params[:page]).order('created_at desc').where(:published => true)
-    # else
+    @articles = Article.paginate(:per_page => 10, :page => params[:page]).order('created_at desc').where(:published => true)
 
     # this is a bit broken, default welcome page shouldn't have a load of results on it.
-      @search = Article.search do
-        fulltext params[:search]
-        with(:is_published, true)
-        facet(:created_month)
-        with(:created_month, params[:month]) if params[:month].present?
-      end
-      @search
-      @articles = @search.results
-    # end
+      # @search = Article.search do
+      #   fulltext params[:search]
+      #   with(:is_published, true)
+      #   facet(:created_month)
+      #   with(:created_month, params[:month]) if params[:month].present?
+      # end
+      # @search
+      # @articles = @search.results
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
