386da8a42e1198749ff3dfd7a17198fb9e9a9fc6
Peter Mellett <bulletraven@gmail.com>
2012-04-14 10:48:41 +0100

    tidied schedule file

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,22 +2,11 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-
-# Example:
 #
-set :output, "$RAILS_HOME/shared/log/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
+
+set :output, "$RAILS_HOME/shared/log/cron_log.log"
+
 every 10.minutes do
   runner 'ActionController::Base.new.expire_fragment("mytweet")'
 end
