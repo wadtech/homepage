b5c5214412057e14d74d470eb5d06e85d15d5bbe
Peter Mellett <bulletraven@gmail.com>
2012-09-01 19:39:01 +0100

    delete weird devise timedout flash key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :delete_timedout_flash
+
+  def delete_timedout_flash
+    flash.delete(:timedout)
+  end
 end
