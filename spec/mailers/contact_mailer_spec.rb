b590fefd86343b39c76bf89a7cd67369eccd80ee
Peter Mellett <bulletraven@gmail.com>
2012-10-07 19:07:35 +0100

    full coverage of contact mailer

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,4 +1,19 @@
 require "spec_helper"
 
 describe ContactMailer do
+  describe "contact" do
+    mail = ContactMailer.contact("Subject", "Content", "127.0.0.1")
+
+    it 'renders the subject' do
+      mail.subject.should eq "Subject"
+    end
+
+    it 'renders the content' do
+      mail.body.encoded.should have_content "Content"
+    end
+
+    it 'contains the IP of the sender' do
+      mail.body.encoded.should have_content '127.0.0.1'
+    end
+  end
 end
