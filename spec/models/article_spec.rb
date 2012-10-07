449acb228179fe27959cb78675aa751612b9dac6
Peter Mellett <bulletraven@gmail.com>
2012-10-07 19:47:47 +0100

    fully covered article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,4 +10,13 @@ describe Article do
   it "is invalid without content" do
     article = FactoryGirl.build(:article, :content => nil).should_not be_valid
   end
+  it "has a well-formatted to_param" do
+    article = FactoryGirl.build(:article, :id => 25, :title => "Article")
+    article.to_param.should eq "25-Article-article"
+  end
+
+  it "has a well-formatted created_month attribute" do
+    article = FactoryGirl.create(:article)
+    article.created_month.should eq Time.now.strftime("%B %Y")
+  end
 end
