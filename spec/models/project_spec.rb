6b93e978594fb137a3aeb3ed6dd5184c85812ff7
Peter Mellett <bulletraven@gmail.com>
2012-11-12 21:06:28 +0000

    passing project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,7 +20,7 @@ describe Project do
     %w[Todo Blog].each do |title|
       FactoryGirl.create(:project, :title => title)
     end
-    Project.find("todo").should_not be_nil
-    Project.find("blog").title.should eq "Blog"
+    Project.find("Todo").title.should eq "Todo"
+    Project.find("Blog").title.should eq "Blog"
   end
 end
