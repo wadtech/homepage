fa517e45233edda484dad620acfb8e6a14cb5cbc
Peter Mellett <bulletraven@gmail.com>
2012-11-23 22:34:06 +0000

    partially upgraded to latest capybara- failing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,13 +43,3 @@ RSpec.configure do |config|
   # ignore tests marked as broken
   config.filter_run_excluding :broken => true
 end
-
-def login_admin
-  @user = FactoryGirl.create(:admin)
-
-  visit new_admin_session_path
-
-  fill_in 'admin_email', :with => @user.email
-  fill_in 'admin_password', :with => @user.password
-  click_button "Sign in"
-end
