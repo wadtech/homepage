2a2166aa832f5ca456d9293c53073bee1f795439
Peter Mellett <bulletraven@gmail.com>
2012-11-28 23:10:12 +0000

    change to use external smtp server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,12 @@ Homepage::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port => 25,
+    :address => Settings.mailer.address,
+    :port => Settings.mailer.port,
+    :authentication => Settings.mailer.authentication,
+    :enable_starttls_auto => Settings.mailer.enable_starttls,
+    :user_name => Settings.mailer.user_name,
+    :password => Settings.mailer.password,
     :domain => Settings.mailer.domain
   }
 
