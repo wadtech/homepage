f9fbde2bbf3026c1385c59cad403359c42b2ec8d
Peter Mellett <bulletraven@gmail.com>
2012-11-28 21:28:55 +0000

    now I forgot some quote marks.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Homepage::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 127.0.0.1,
+    :address => "127.0.0.1",
     :port => 25,
     :domain => Settings.mailer.domain
   }
