7c2907edf165662afb1c96f9976bddd5e0373644
Peter Mellett <bulletraven@gmail.com>
2013-01-03 19:29:36 +0000

    add logging and post hook to unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,5 @@
+Configurator::DEFAULTS[:logger].formatter = Logger::Formatter
+
 root = ENV['RAILS_ROOT']
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
@@ -7,3 +9,9 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.blog.sock"
 worker_processes 2
 timeout 30
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  child_pid = server.config[:pid].sub('.pid', ".#{worker.nr}.pid")
+  system("echo #{Process.pid} > #{child_pid}")
+end
