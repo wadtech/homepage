407c02ca65d57bbe17701064c0627eb68150d0d2
Peter Mellett <bulletraven@gmail.com>
2013-03-03 12:13:46 +0000

    refactorings

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-#My homepage blog thing
+# Homepage
+
+A simple cms-y thing in Rails.
+
 [![Build Status](https://secure.travis-ci.org/wadtech/homepage.png)](http://travis-ci.org/wadtech/homepage)
 [![Dependency Status](https://gemnasium.com/wadtech/homepage.png)](https://gemnasium.com/wadtech/homepage)
-
-Continuing my mission to learn Rails.
+[![Code Climate](https://codeclimate.com/github/wadtech/homepage.png)](https://codeclimate.com/github/wadtech/homepage)
 
 ##Technical Specs
 
@@ -10,3 +12,25 @@ Continuing my mission to learn Rails.
 * Devise
 * redcarpet 2 for markdown rendering
 * PostgreSQL
+
+## License
+
+Copyright (c) 2012 Peter Mellett
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @articles }
+      format.html
     end
   end
 
@@ -19,8 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
+      format.html
     end
   end
 
@@ -30,8 +28,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @article }
+      format.html
     end
   end
 
@@ -48,10 +45,8 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render json: @article, status: :created, location: @article }
       else
         format.html { render action: "new" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -64,10 +59,8 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       if @article.update_attributes(params[:article])
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -80,14 +73,13 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to articles_url }
-      format.json { head :ok }
     end
   end
 
  def toggle_publish
     @article = Article.find(params[:id])
     @article.toggle!(:published)
-    
+
     respond_to do |format|
       format.js
     end
