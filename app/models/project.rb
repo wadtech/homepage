ef719de3e1647d69692b11f330223339b39af9da
Peter Mellett <bulletraven@gmail.com>
2013-06-28 22:30:17 +0100

    finish moving to rails4

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,23 @@
 class Project < ActiveRecord::Base
   include PgSearch
-  multisearchable :against => [:title, :description, :summary]
-  pg_search_scope :search, :against => [:title, :description, :summary]
-  pg_search_scope :search_by_title, :against => :title
-  pg_search_scope :search_by_content, :against => [:description, :summary]
+  multisearchable against: [:title, :description, :summary]
+  pg_search_scope :search, against: [:title, :description, :summary]
+  pg_search_scope :search_by_title, against: :title
+  pg_search_scope :search_by_content, against: [:description, :summary]
 
   has_many :highlights
   accepts_nested_attributes_for :highlights, allow_destroy: true
 
-  has_attached_file :image, :styles => { :large => "400x400>", :medium => "300x300>", :thumb => "200x200>" }
+  has_attached_file :image, styles: { large: "400x400>", medium: "300x300>", thumb: "200x200>" }
 
-  validates :title, :description, :presence => true
+  validates :title, :description, presence: true
 
   def to_param
     self.title.parameterize
   end
 
   def self.find(input)
-    input.to_i == 0 ? first(:conditions => ["title ILIKE ?", input.gsub("-", " ")]) : super
+    #to_i converts strings that start with a letter to 0
+    input.to_i == 0 ? where("title ILIKE ?", input.gsub("-", " ")).first : super
   end
 end
