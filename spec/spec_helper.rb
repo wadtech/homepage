9a6a37d5d3a2d2b07fe571e71fcb539b46867212
Peter Mellett <pjm@petermellett.co.uk>
2015-03-20 22:43:24 +0000

    pass my build, passsss

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+# maybe fix travis-ci weirdness
+Capybara.default_wait_time = 15
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
