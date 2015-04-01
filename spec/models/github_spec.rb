c8be9aef34c04807bbe49dfb91d07ed4be622f84
Peter Mellett <pjm@petermellett.co.uk>
2015-04-01 21:22:14 +0100

    use the probably unwise but super convenient vcr rspec config snippet

--- spec/models/github_spec.rb
+++ spec/models/github_spec.rb
@@ -4,22 +4,18 @@ describe Github do
   let (:subject) { described_class.new }
 
   it "lists public activity" do
-    VCR.use_cassette('public_activity', match_requests_on: [:host, :path]) do
-      resp = subject.public_activity
-      expect(resp.size).to eq(30)
+    resp = subject.public_activity
+    expect(resp.size).to eq(30)
 
-      expect(resp.first[:type]).to eq('PushEvent')
-    end
+    expect(resp.first[:type]).to eq('PushEvent')
   end
 
   it "lists public source repositories only" do
-    VCR.use_cassette('repos') do
-      resp = subject.repositories
+    resp = subject.repositories
 
-      resp.each do |repo|
-        expect(repo[:fork]).to eq(false)
-        expect(repo[:owner][:login]).to eq('wadtech')
-      end
+    resp.each do |repo|
+      expect(repo[:fork]).to eq(false)
+      expect(repo[:owner][:login]).to eq('wadtech')
     end
   end
 end
