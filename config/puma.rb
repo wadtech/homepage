ce715185ab1d72ded58895a511075e106b6b0d77
Peter Mellett <pjm@petermellett.co.uk>
2015-04-07 22:13:12 +0100

    apparently I need these

--- config/puma.rb
+++ config/puma.rb
@@ -9,6 +9,10 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+pidfile "#{ENV['RAILS_ROOT']}/tmp/puma/pid"
+state_path "#{ENV['RAILS_ROOT']}/tmp/puma/state"
+activate_control_app
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
