82be8b8145ce6b4117089ee99d1a8aa7efca56d9
Peter Mellett <pjm@petermellett.co.uk>
2015-04-13 21:30:03 +0100

    pumaaaa

--- config/puma.rb
+++ config/puma.rb
@@ -6,14 +6,12 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+bind "unix://#{ENV['RAILS_ROOT']}/tmp/puma/sock"
 pidfile "#{ENV['RAILS_ROOT']}/tmp/puma/pid"
 state_path "#{ENV['RAILS_ROOT']}/tmp/puma/state"
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
